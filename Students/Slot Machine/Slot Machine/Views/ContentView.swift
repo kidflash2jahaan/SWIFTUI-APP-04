5eac33da6b856577950a0cf017f5d12410285ccc
kidflash2jahaan <53150988+kidflash2jahaan@users.noreply.github.com>
2022-06-19 15:25:21 -0700

    beta 2

--- Students/Slot Machine/Slot Machine/Views/ContentView.swift
+++ Students/Slot Machine/Slot Machine/Views/ContentView.swift
@@ -10,9 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
-            Text("Hello, World!")
+            VStack(alignment: .center, spacing: 5) {
+                Image("gfx-slot-machine")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(minWidth: 256, idealWidth: 300, maxWidth: 320, minHeight: 112, idealHeight: 130, maxHeight: 140, alignment: .center)
+                    .padding(.horizontal)
+                    .layoutPriority(1)
+                    .shadow(color: Color("ColorTransparentBlack"), radius: 0, x: 0, y: 6)
+            }
+            .padding()
+            .frame(maxWidth: 720)
         }
     }
 }

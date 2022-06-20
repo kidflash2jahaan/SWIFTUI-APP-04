4218cbaa78da641f1b974ba021f48262af089ea7
kidflash2jahaan <53150988+kidflash2jahaan@users.noreply.github.com>
2022-06-20 08:59:46 -0700

    beta 3

--- Students/Slot Machine/Slot Machine/Views/ContentView.swift
+++ Students/Slot Machine/Slot Machine/Views/ContentView.swift
@@ -13,14 +13,56 @@ struct ContentView: View {
             LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
             VStack(alignment: .center, spacing: 5) {
-                Image("gfx-slot-machine")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(minWidth: 256, idealWidth: 300, maxWidth: 320, minHeight: 112, idealHeight: 130, maxHeight: 140, alignment: .center)
-                    .padding(.horizontal)
-                    .layoutPriority(1)
-                    .shadow(color: Color("ColorTransparentBlack"), radius: 0, x: 0, y: 6)
+                LogoView()
+                
+                Spacer()
+                
+                HStack {
+                    HStack {
+                        Text("Your\nCoins".uppercased())
+                            .scoreLabelStyle()
+                            .multilineTextAlignment(.trailing)
+                        
+                        Text("100")
+                            .scoreNumberStyle()
+                            .modifier(ScoreNumberModifier())
+                    }
+                    .modifier(ScoreContainerModifier())
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text("200")
+                            .scoreNumberStyle()
+                            .modifier(ScoreNumberModifier())
+                        
+                        Text("High\nScore".uppercased())
+                            .scoreLabelStyle()
+                            .multilineTextAlignment(.leading)
+                    }
+                    .modifier(ScoreContainerModifier())
+                }
+                
+                Spacer()
             }
+            .overlay(
+                Button(action: {
+                    print("Reset the game")
+                }) {
+                    Image(systemName: "arrow.uturn.backward.circle")
+                }
+                    .modifier(ButtonModifier()),
+                alignment: .topLeading
+            )
+            .overlay(
+                Button(action: {
+                    print("Info View")
+                }) {
+                    Image(systemName: "info.circle")
+                }
+                    .modifier(ButtonModifier()),
+                alignment: .topTrailing
+            )
             .padding()
             .frame(maxWidth: 720)
         }
